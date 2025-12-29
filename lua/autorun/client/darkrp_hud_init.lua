76ae8aa320d3d007e5c2ad2da5989107ed6c627c
merq757 <antonovivan1709@yandex.ru>
2025-12-29 18:23:34 +0300

    Add client autorun initialization file

---
+++ lua/autorun/client/darkrp_hud_init.lua
@@ -0,0 +1,12 @@
+--[[
+	DarkRP Modern HUD - Auto Initialization
+	Client-side autorun file
+]]--
+
+print("[DarkRP HUD] Loading Modern HUD System...")
+
+-- Include core library
+include("darkrp_hud/core.lua")
+include("darkrp_hud/config.lua")
+
+print("[DarkRP HUD] System loaded successfully!")
